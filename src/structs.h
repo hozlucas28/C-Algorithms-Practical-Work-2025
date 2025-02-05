b629d96e9fccddbe3cb1faa968ff2fdfd1ccf79f
hozlucas28 <hozlucas28@gmail.com>
2025-02-04 21:41:11 -0300

    chore: create `structs.h`

---
+++ src/structs.h
@@ -0,0 +1,11 @@
+
+#ifndef SRC_STRUCTS_H_INCLUDED
+#define SRC_STRUCTS_H_INCLUDED
+
+#include "./macros.h"
+
+typedef struct {
+    char name[TEAM_NAME_LENGTH];
+} Team;
+
+#endif  // SRC_STRUCTS_H_INCLUDED
