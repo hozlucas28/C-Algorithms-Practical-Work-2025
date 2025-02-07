447dcf8f66d41ebb537dde6234a62c1b8d558001
hozlucas28 <hozlucas28@gmail.com>
2025-02-06 22:54:19 -0300

    feature: create `structs.h`

---
+++ libs/structs.h
@@ -0,0 +1,12 @@
+
+#ifndef LIBS_STRUCTS_H_INCLUDED
+#define LIBS_STRUCTS_H_INCLUDED
+
+#include <stdlib.h>
+
+typedef struct {
+    size_t x;
+    size_t y;
+} Position;
+
+#endif  // LIBS_STRUCTS_H_INCLUDED
