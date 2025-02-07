05aa8b230ff36adb4ab6dfd0b6a3f82d1ef95177
hozlucas28 <hozlucas28@gmail.com>
2025-02-06 23:00:55 -0300

    feature: create new structs
    Created `Team`, `Player`, and `APIPlayer` structs.

--- src/macros.h
+++ src/macros.h
@@ -1,10 +1,18 @@
 #ifndef SRC_MACROS_H_INCLUDED
 #define SRC_MACROS_H_INCLUDED
 
+/* --------------------------------- Common --------------------------------- */
+
 #define DEVELOPMENT_TEAM "SQUASH"
 
 #define TEAM_NAME_LENGTH 32
 
 #define CONFIGURATION_PATH "./statics/configuration.txt"
+
+/* --------------------------------- Player --------------------------------- */
+
+#define PLAYER_NAME_LENGTH 64
+
+#define PLAYER_LAST_GAME_PLAYED_LENGTH 32
 
 #endif  // SRC_MACROS_H_INCLUDED
--- src/structs.h
+++ src/structs.h
@@ -2,10 +2,29 @@
 #ifndef SRC_STRUCTS_H_INCLUDED
 #define SRC_STRUCTS_H_INCLUDED
 
+#include <stdlib.h>
+
 #include "./macros.h"
 
+/* --------------------------------- Common --------------------------------- */
+
 typedef struct {
-    char name[TEAM_NAME_LENGTH];
+    char name[TEAM_NAME_LENGTH];  // TODO: use a dynamic method.
 } Team;
+
+/* --------------------------------- Player --------------------------------- */
+
+typedef struct {
+    char name[PLAYER_NAME_LENGTH];  // TODO: use a dynamic method.
+    size_t points;
+    char assignedForm;
+    size_t remainingTurns;
+} Player;
+
+typedef struct {
+    char name[PLAYER_NAME_LENGTH];  // TODO: use a dynamic method.
+    int points;
+    char lastGamePlayed[PLAYER_LAST_GAME_PLAYED_LENGTH];  // TODO: use a dynamic method.
+} APIPlayer;
 
 #endif  // SRC_STRUCTS_H_INCLUDED
