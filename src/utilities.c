cab84ffa2903b1940a7dfc61cc95f59b25b5751f
hozlucas28 <hozlucas28@gmail.com>
2025-02-15 04:36:58 -0300

    feature: create cmp players points
    Co-authored-by: Valentin Massa <valeenmassa2002@gmail.com>

--- src/utilities.c
+++ src/utilities.c
@@ -18,3 +18,9 @@ void addPlayerPoints(Player *player, int points) { player->points += points; }
 void addPlayerGamesWons(Player *player) { player->gamesWons += 1; }
 void addPlayerLostGames(Player *player) { player->lostGames += 1; }
 void addPlayerTiedGames(Player *player) { player->tiedGames += 1; }
+
+int cmpPlayersAscPoints(const void *playerA, const void *playerB) {
+    Player *_playerA = (Player *)playerA;
+    Player *_playerB = (Player *)playerB;
+    return _playerB->points - _playerA->points;
+}
